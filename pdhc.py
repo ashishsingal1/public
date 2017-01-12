9979e78c357f3b0b7b100d3acd8110a4075a723d
asingal <ashish.singal1@gmail.com>
2017-01-12 11:09:04 -0500

    minor updates

--- pdhc.py
+++ pdhc.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import highcharts as hc
 
-def df_to_hc(df):
+def df_to_hc(df, chart_type='bar'):
     H = hc.Highchart(width=750, height=600)
     options = {
         'xAxis': {
@@ -11,5 +11,5 @@ def df_to_hc(df):
     }
     H.set_dict_options(options)
     # use .tolist() instead of list() to convert numpy to native types
-    for r in list(df.index): H.add_data_set(df.loc[r].tolist(), 'bar', r)
+    for r in list(df.index): H.add_data_set(df.loc[r].tolist(), chart_type, r)
     return H
